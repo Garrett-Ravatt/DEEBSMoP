a9e055ac4203907b63d6123f14d1c4795c1cb4ce
Garrett-Ravatt <gravatt@calpoly.edu>
2024-01-16 14:54:37 -0800

    emeny spawner

---
+++ objects/enemies/emeny_spawner.gd
@@ -0,0 +1,16 @@
+extends Marker2D
+
+var emeny = preload("res://objects/enemies/emeny_2.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	emeny.instantiate()
